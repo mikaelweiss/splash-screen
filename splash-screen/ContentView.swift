13a8770b57b3c111bfd1911c6c5e46d7899166f4
Mikael Weiss <campingmikael@icloud.com>
2026-03-07 08:59:17 -0700

    Initial Commit

---
+++ splash-screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  splash-screen
+//
+//  Created by Mikael Weiss on 3/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ splash-screen/splash_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  splash_screenApp.swift
+//  splash-screen
+//
+//  Created by Mikael Weiss on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct splash_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
